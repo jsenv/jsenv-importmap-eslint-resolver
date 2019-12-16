b75f19a400faa597b2c1c32cbf4d24368b4ac71c
Damien Maillard <damien.maillard@dailymotion.com>
2019-12-16 14:30:24 +0100

    fix eslint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,9 @@
-const { createConfig } = require("@jsenv/eslint-config")
+const { createEslintConfig } = require("@jsenv/eslint-config")
+const jsenvConfig = require("./jsenv.config.js")
 
-const config = createConfig({
-  projectDirectoryPath: __dirname,
+const config = createEslintConfig({
+  ...jsenvConfig,
   importResolutionMethod: "import-map",
-  // importResolverFilePath: `${__dirname}/dist/commonjs/main.js`
-  // importResolverOptions: { logLevel: 'debug', node: true }
 })
-
-// config.settings["import/resolver"] = {
-//   [`${__dirname}/dist/commonjs/main.js`]: {
-//     projectDirectoryPath: __dirname,
-//     logLevel: "info",
-//     node: true,
-//   },
-// }
 
 module.exports = config
